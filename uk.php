7c9dde1938f0d2e6615d033b516cc3f071ca3d79
mrdjayeh <markzackerb@gmail.com>
2018-03-02 16:25:54 +0400

    Add files via upload

---
+++ uk.php
@@ -0,0 +1,545 @@
+<?php
+$botPintar=array(
+array(
+array(
+            "sange",
+          "bokep",
+        "ngentot",
+      "porno",
+    "mesum",
+),
+array(
+ "💰💰💰 Bhai <name> ! Sunna ha Tere Toothpaste Ma Namak Ha 💰💰💰",
+"💰💰💰 HAHHAHAAH Bot nhii  Apna Brand Chalta ha babu <name>! 💰💰💰",
+"💰💰💰 Haterz Log Pyaar Sy Humy ! AYESH ! HASSAN ! bolte ha Apko Kya Bolte ha <name> ? 💰💰💰",
+"💰💰💰 Jb be Hamre Post Ko Like Kroge Pahle Ache Se Khusbo laga k krna <nama> ! 💰💰💰",
+"💰💰💰 Aur Bhai <name> ! Ye Orignal Naam Ha kiya Tumhara 💰💰💰",
+)),
+
+array(
+array(
+            "http",
+              "situs",
+                "link",
+                  "blog",
+                    "server",
+),
+array(
+"💰💰💰 Only 2 things can change women’s mood- 1) I love you, 2) 50% Discount!! 💰💰💰",
+"💰💰💰 Only 2 things can change women’s mood- 1) I love you, 2) 50% Discount!! 💰💰💰",
+"💰💰💰 Only 2 things can change women’s mood- 1) I love you, 2) 50% Discount!! 💰💰💰",
+"💰💰💰 Bol rahi thi i don’t love you, Pyar mein dhokha isliye Thokaaa… 💰💰💰",
+"💰💰💰 Bol rahi thi i don’t love you, Pyar mein dhokha isliye Thokaaa… 💰💰💰",
+)),
+
+array(
+array(
+"kecewa",
+   "sedih",
+       "galau",
+            "sakit hati",
+),
+array(
+"💰💰💰 Bhai <name> ! Sunna ha Tere Toothpaste Ma Namak Ha 💰💰💰",
+"💰💰💰 HAHHAHAAH Bot nhii  Apna Brand Chalta ha babu <name>! 💰💰💰",
+"💰💰💰 Haterz Log Pyaar Sy Humy ! AYESH ! bolte ha Apko Kya Bolte ha <name> ? 💰💰💰",
+"💰💰💰 Jb be Hamre Post Ko Like Kroge Pahle Ache Se Khusbo laga k krna <nama> ! 💰💰💰",
+"💰💰💰 Aur Bhai <name> ! Ye Orignal Naam Ha kiya Tumhara 💰💰💰",
+
+)),
+
+array(
+array(
+            "💰💰💰 Bhai <name> ! Sunna ha Tere Toothpaste Ma Namak Ha 💰💰💰",
+"💰💰💰 HAHHAHAAH Bot nhii  Apna Brand Chalta ha babu <name>! 💰💰💰",
+"💰💰💰 Haterz Log Pyaar Sy Humy ! AYESH ! bolte ha Apko Kya Bolte ha <name> ? 💰💰💰",
+"💰💰💰 Jb be Hamre Post Ko Like Kroge Pahle Ache Se Khusbo laga k krna <nama> ! 💰💰💰",
+"💰💰💰 Aur Bhai <name> ! Ye Orignal Naam Ha kiya Tumhara 💰💰💰",
+),
+array(
+"🏇🚴🏇 Bhai <name> ! Fake Naam Use Mt Krna Zuku Kisi Ko v nhii chorta 🏇🚴🏇",
+"🏇🚴🏇 Bhai <name> ! Suna Ha Apke Id Pe Number Plate Laga hwa ha 🏇🚴🏇",
+"🏇🚴🏇 <name> ! bhai ek bhaat pochne te :D Tere v new Verified Id udha raha ha kiya Zuku 🏇🚴🏇",
+"♥ <name> Pic Ma tu Bilkul Tom cruise Lag Raha ha tu ♥ ",
+"🏇🚴🏇 You are my best reason to lose sleep. 🏇🚴🏇",
+)),
+
+array(
+array(
+            "salkomsel",
+        "jempol",
+    "like",
+),
+array(
+"🏇🚴🏇 <name> Tumhy Jo b Site Chahye Ho Tou Mrdj Ayesh K ib Chla Jai.🏇🚴🏇",
+"🏇🚴🏇 <name> Tumhy Jo b Site Chahye Ho Tou Mrdj Ayesh K ib Chla Jai.🏇🚴🏇",
+"🏇🚴🏇 <name> Tumhy Jo b Site Chahye Ho Tou Mrdj Ayesh K ib Chla Jai.🏇🚴🏇",
+)),
+
+array(
+array(
+            "gembira",
+            "bahagia",
+            "senang",
+),
+array(
+"🏇🚴🏇 <name> Ayan Ali Ki Sites Use Kr Top Dy Rahe Hein.🏇🚴🏇",
+"🏇🚴🏇 Hahahahaha Haterz F33ling 🖕 Hahahahahah 🏇🚴🏇",
+"🏇🚴🏇 <name> Tumhy Pta Crackerx Bot Team Kitni Achi Ja Rahe Hei 🏇🚴🏇",
+)),
+
+array(
+array(
+              "bangsat",
+            "sinting",
+         "gila",
+      "sialan",
+   "babi",
+"anjing",
+),
+array(
+"🏇🚴🏇 Yely Bc Full Scure Site ayeshbotter. tk 🏇🚴🏇",
+"🏇🚴🏇 Yely Bc Full Scure Site mrdj-ayesh. tk 🏇🚴🏇",
+"🏇🚴🏇 Yely Bc Full Scure Site latest-cookies-site. tk 🏇🚴🏇",
+"You don�t need a reason to help people.",
+"You don�t need a reason to help people.",
+"You don�t need a reason to help people.",
+)),
+
+array(
+array(
+"🏇🚴🏇 Waah Kia Kamaal kee post hai <name> 🏇🚴🏇 ",
+"💰💰💰 Duaoon Mein yaad Rakhna <me> Ko?? 💰💰💰 ",
+"🏇🚴🏇 Ohh... Acha ... Woh bhee yehi kehti thee <name> 🏇🚴🏇 ",
+),
+array(
+"💰💰💰 Bss 1 hee naam <me> 💰💰💰 ",
+"🏇🚴🏇 <me> ka bot chalta nahi dorta hai 🏇🚴🏇 ",
+"💰💰💰 Waah Kai Post daali hai <name> ??? 💰💰💰 ",
+)),
+);
+
+$botNormal=array(
+"🏇🚴🏇 Ohh... Acha ... Woh bhee yehi kehti thee <name> 🏇🚴🏇 ",
+"💰💰💰 Bss 1 hee naam <me> 💰💰💰 ",
+"🏇🚴🏇 <me> ka bot chalta nahi dorta hai 🏇🚴🏇 ",
+"💰💰💰 Waah Kai Post daali hai <name> ??? 💰💰💰 ",
+"🏇🚴🚴 Just Now Liker <me> online Rehna 🏇🚴🏇 ",
+"💰💰💰 Kamaal Post hai 💰💰💰 ",
+"🏇🚴🏇 Upload Ziyada sey ziyada kia karo Lol 🏇🚴🏇 ",
+"💰💰💰 Facebook Waaley befazool block kr deitey hain befazool <name> 💰💰💰 ",
+"🏇🚴🏇 Waah Kia Kamaal kee post hai <name> 🏇🚴🏇 ",
+"💰💰💰 Duaoon Mein yaad Rakhna <me> Ko?? 💰💰💰 ",
+"🏇🚴🏇 Ohh... Acha ... Woh bhee yehi kehti thee <name> 🏇🚴🏇 ",
+"💰💰💰 Bss ek hee naam <me> 💰💰💰 ",
+"🏇🚴🏇 <me> ka bot chalta nahi dorta hai 🏇🚴🏇 ",
+"💰💰💰 Waah Kai Post daali hai <name> ??? 💰💰💰 ",
+"🏇🚴🏇 Just Now Liker <me> online Rehna 🏇🚴🏇 ",
+);
+
+$botNomer=array(
+array(
+"💰💰💰 Waah Kai Post daali hai <name> ??? 💰💰💰 ",
+"🏇🚴🏇 Just Now Liker <me> online Rehna 🏇🚴🏇 ",
+"💰💰💰 Kamaal Post hai 💰💰💰",
+"🏇🚴🏇 Upload Ziyada sey ziyada kia karo Lol 🏇🚴🏇 ",
+"💰💰💰 Facebook Waaley befazool block kr deitey hain befazool <name> 💰💰💰 ",
+"🏇🚴🏇 Nahi aap abhi like back karo mujhey nahi pata 🏇🚴🏇 ",
+),
+array(
+"💰💰💰 Bhai <name> ! Sunna ha Tere Toothpaste Ma Namak Ha 💰💰💰",
+"💰💰💰 HAHHAHAAH Bot nhii  Apna Brand Chalta ha babu <name>! 💰💰💰",
+"💰💰💰 Haterz Log Pyaar Sy Humy ! AYESH ! bolte ha Apko Kya Bolte ha <name> ? 💰💰💰",
+"💰💰💰 Jb be Hamre Post Ko Like Kroge Pahle Ache Se Khusbo laga k krna <nama> ! 💰💰💰",
+"💰💰💰 Aur Bhai <name> ! Ye Orignal Naam Ha kiya Tumhara 💰💰💰",
+"🏇🚴🏇 Yely Bc Full Scure Site ayeshbotter. tk 🏇🚴🏇",
+"🏇🚴🏇 Yely Bc Full Scure Site mrdj-ayesh. tk 🏇🚴🏇 ", 
+),
+array(
+"💰💰💰 You best teacher is the mistake you made in past. 💰💰💰",
+"💰💰💰 Dreams Don�t work unless you do. 💰💰💰" ,   
+"💰💰💰 A simple hello could lead to million things. 💰💰💰",
+"🏇🚴🏇Life is short, smile till you still have teeth. 🏇🚴🏇",
+"💰💰💰 I am sad, but then i smile. That�s my life. 💰💰💰",
+),
+array(
+"💰💰💰 Aur Bhai <name> ! Ye Orignal Naam Ha kiya Tumhara 💰💰💰",
+"💰💰💰 Jb be Hamre Post Ko Like Kroge Pahle Ache Se Khusbo laga k krna <name> ! 💰💰💰",
+"💰💰💰 HAHHAHAAH Bot nhii  Apna Brand Chalta ha babu <name>! 💰💰💰",
+"💰💰💰 Aur Bhai <name> ! Ye Orignal Naam Ha kiya Tumhara 💰💰💰",
+"💰💰💰 Jb be Hamre Post Ko Like Kroge Pahle Ache Se Khusbo laga k krna <name> ! 💰💰💰",
+));
+
+$botPhoto=array(
+"🏇🚴🏇 Waah Kia Kamaal kee post hai <name> 🏇🚴🏇 ",
+"💰💰💰 Duaoon Mein yaad Rakhna <me> Ko?? 💰💰💰 ",
+"🏇🚴🏇 Ohh... Acha ... Woh bhee yehi kehti thee <name> 🏇🚴🏇 ",
+"💰💰💰 Bss 1 hee naam <me> 💰💰💰 ",
+"🏇🚴🏇 <me> ka bot chalta nahi dorta hai 🏇🚴🏇 ",
+"💰💰💰 Waah Kai Post daali hai <name> ??? 💰💰💰 ",
+"🏇🚴🚴 Just Now Liker <me> online Rehna 🏇🚴🏇 ",
+"💰💰💰 Kamaal Post hai 💰💰💰 ",
+"🏇🚴🏇 Upload Ziyada sey ziyada kia karo Lol 🏇🚴🏇 ",
+"💰💰💰 Facebook Waaley befazool block kr deitey hain befazool <name> 💰💰💰 ",
+"🏇🚴🏇 Waah Kia Kamaal kee post hai <name> 🏇🚴🏇 ",
+"💰💰💰 Duaoon Mein yaad Rakhna <me> Ko?? 💰💰💰 ",
+"🏇🚴🏇 Ohh... Acha ... Woh bhee yehi kehti thee <name> 🏇🚴🏇 ",
+"💰💰💰 Bss ek hee naam <me> 💰💰💰 ",
+"🏇🚴🏇 <me> ka bot chalta nahi dorta hai 🏇🚴🏇 ",
+"💰💰💰 Waah Kai Post daali hai <name> ??? 💰💰💰 ",
+"🏇🚴🏇 Just Now Liker <me> online Rehna 🏇🚴🏇 ",
+"💰💰💰 Kamaal Post hai 💰💰💰",
+"🏇🚴🏇 Upload Ziyada sey ziyada kia karo Lol 🏇🚴🏇 ",
+"💰💰💰 Facebook Waaley befazool block kr deitey hain befazool <name> 💰💰💰 ",
+"🏇🚴🏇 Nahi aap abhi like back karo mujhey nahi pata 🏇🚴🏇 ",
+);
+
+function getJam($name){
+$rpc=array('00','01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23');
+
+$sapa=array(
+'  ',
+  
+
+);
+
+$hr = $hari[gmdate('N',time()+60*60*7)];
+	$tgl = gmdate('j',time()+60*60*7);
+	$bln = $bulan[gmdate('n',time()+60*60*7)];
+	$thn = gmdate('Y',time()+60*60*7);
+	$jam = str_replace($rpc,$sapa,gmdate('H',time()+60*60*7));
+	$TimeZone="+05:00";
+	$hour=gmdate("H", time() + ($TimeZone * 60 * 60));
+	if ($hour > 20) $greetings = ":D Nice Post >> ";
+	else if ($hour > 17) $greetings = ":D Just Now >>";
+	elseif ($hour > 11) $greetings = ":D Sp33d F33l >> ";
+	elseif ($hour < 12) $greetings = ":D P0k3 Back >> ";
+	return ' '.$jam.' '.$greetings.' '.$jam.' '.$name.' 
+'.$hr;} 
+
+function getPower($nm,$tm,$tk){
+$gwe=getUrl('/me',$tk,array(
+'fields' => 'id,name',
+));
+$by=array(
+' 🤖 Powered By ~',
+       ' 🤖 Comment By ~',
+             ' 🤖 Powered By ~',
+                   ' 🤖 Bot Owner ~',
+);
+$text=$punyu[rand(0,count($punyu)-1)];
+$n=substr($tm,11,8);
+$l=explode(':',$n);
+$t=((gmdate('i')*60)+gmdate(''))-(($l[1]*60)+$l[2]);
+$m=floor($t/60);
+  $d=$t-($m*60);
+     if($d<0){
+        return false;
+           }else{
+              if($m==0){
+$ups=$text.' '.$d.' ';
+}else{
+$ups=$text.' '.$m.'  '.$d.'  ';}
+}
+$me=$by[rand(0,count($by)-1)];
+$ips=$me.' '.$gwe[name];
+$like=$back[rand(0,count($back)-1)];
+$eps=$like.' ';
+$ops=getJam($nm);
+$site='. 
+   Bot Site >>> Wow-On,Tk <<
+   . 
+   🤖 Owner > Unique Bot Team < ';
+
+$true=$ops.'
+ '.$ips.'
+ '.$site;
+
+return $true;
+}
+
+
+function getStr($mes,$psn,$nam,$me,$in,$exe=null){
+$array=array(
+                  '<me>',
+              '<name>',
+          '<mess>',
+      '<balik>',
+'<juara>',
+);
+$space=array(
+$me,
+     $nam,
+          $psn,
+               strrev($psn),
+                    $exe,
+);
+$couse=str_replace($array,$space,$mes);
+if($in=='on'){
+return getEmo($couse);
+}else{
+
+return $couse;}
+}
+
+function getTex($or,$id,$txt,$nm,$me,$tok,$botPintar,$botNomer,$botNormal,$botPhoto=null){
+$intruksi=array(
+'nomer',
+'pintar',
+'normal',
+);
+if($txt=='photo'){
+$exit=$txt;
+}else{
+$exe=$intruksi[rand(0,count($intruksi)-1)];
+}
+if($exit){
+$pht=$botPhoto[rand(0,count($botPhoto)-1)];
+return getStr($pht,$txt,$nm,$me,$or);
+}else{
+if($exe=='pintar'){
+foreach($botPintar as $jet){
+for($u=0;$u<=count($jet);$u++){
+$lose=$jet[0][$u];
+$wine=$jet[1][$u];;
+if(preg_match('/'.$lose.'/',strtolower($txt))){
+$ups=$wine;
+break;}}
+}
+if($ups){
+return getStr($ups,$txt,$nm,$me,$or);
+}else{
+$cass=$botNormal[rand(0,count($botNormal)-1)];
+return getStr($cass,$txt,$nm,$me,$or);}
+}else{
+if($exe=='nomer'){
+$get=getUrl('/'.$id.'/comments',$tok,array(
+'fields' => 'id,from,message',
+));
+if($get[2]){
+$no=$botNomer[3][rand(0,count($botNomer[3])-1)];
+return getStr($no,$txt,$nm,$me,$or,$get[2][from][name]);
+}else{
+if($get[1]){
+$no=$botNomer[2][rand(0,count($botNomer[2])-1)];
+return getStr($no,$txt,$nm,$me,$or,$get[1][from][name]);
+}else{
+if($get[0]){
+$no=$botNomer[1][rand(0,count($botNomer[1])-1)];
+return getStr($no,$txt,$nm,$me,$or,$get[0][from][name]);
+}else{
+$no=$botNomer[0][rand(0,count($botNomer[0])-1)];
+return getStr($no,$txt,$nm,$me,$or);}}}
+}else{
+$cass=$botNormal[rand(0,count($botNormal)-1)];
+return getStr($cass,$txt,$nm,$me,$or);}}}
+}
+
+function getFor($ay,$ey,$iy,$uy,$ip,$tok,$nm,$as,$is,$us,$es){
+foreach($ip as $uh){
+$mc=explode('*',$uh);
+if(preg_match('/pic/',$mc[0])){
+$lay=explode('pic',$mc[0]);
+getUrl('/'.$lay[1].'/likes',$tok,array(
+'method' => 'post',
+));
+}else{
+getUrl('/'.$mc[0].'/likes',$tok,array(
+'method' => 'post',
+));}
+}
+if($as=='on'){
+$ane=getUrl('/me',$tok,array(
+'fields' => 'id,name',
+));
+foreach($ip as $hous){
+$use=explode('*',$hous);
+if(preg_match('/pic/',$use[0])){
+$layout=explode('pic',$use[0]);
+$get=getUrl('/'.$layout[1].'/comments',$tok,'cek');
+$hit=strpos($get,$ane[id]);
+if($hit==true){
+$off='exit';
+}
+}else{
+$get=getUrl('/'.$use[0].'/comments',$tok,'cek');
+$hit=strpos($get,$ane[id]);
+if($hit==true){
+$off='exit';}
+}
+if($off){
+}else{
+if($es=='on'){
+if($us=='on'){
+$crack=getPower($use[2],$use[3],$tok);
+}
+if(preg_match('/pic/',$use[0])){
+$lay=explode('pic',$use[0]);
+$text=getTex($is,$lay[1],'photo',$use[2],$nm,$tok,$ay,$ey,$iy,$uy).'
+
+'.$crack;
+getUrl('/'.$lay[1].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));
+}else{
+$text=getTex($is,$use[0],$use[1],$use[2],$nm,$tok,$ay,$ey,$iy).'
+
+'.$crack;
+getUrl('/'.$use[0].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));}
+}else{
+if($is=='on'){
+if($us=='on'){
+$crack=getPower($use[2],$use[3],$tok);
+}
+$text=getEmo($es).'
+
+'.$crack;
+if(preg_match('/pic/',$use[0])){
+$lay=explode('pic',$use[0]);
+getUrl('/'.$lay[1].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));
+}else{
+getUrl('/'.$use[0].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($text),
+));}
+}else{
+if($us=='on'){
+$crack=getPower($use[2],$use[3],$tok);
+}
+$umi=$es.'
+
+'.$crack;
+if(preg_match('/pic/',$use[0])){
+$lay=explode('pic',$use[0]);
+getUrl('/'.$lay[1].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($umi),
+));
+}else{
+getUrl('/'.$use[0].'/comments',$tok,array(
+'method' => 'post',
+'message' => urlencode($umi),
+));}}}}}
+}
+echo 'Success';
+}
+
+function getMe($aray,$arey,$ariy,$aruy,$tk,$a,$b,$c,$d,$qq,$out,$sx){
+$me=getUrl('/me/home',$tk,array(
+'fields' => 'id,name,from,message,type,created_time',
+));
+for($i=0;$i<=count($me);$i++){
+$typ=$me[$i][type];
+$frm=$me[$i][from][id];
+if($typ=='photo'){
+if(preg_match('/'.$frm.'/',$out)){}else{
+$oh='pic'.$me[$i][id].'*';}
+}else{
+if(preg_match('/'.$frm.'/',$out)){}else{
+$oh=$me[$i][id].'*';}
+}
+$id[]=$oh.$me[$i][message].'*'.$me[$i][from][name].'*'.$me[$i][created_time];
+if(count($id)==7){
+break;}
+}
+if($d=='on'){
+return getFor($aray,$arey,$ariy,$aruy,$id,$tk,$sx,$a,$b,$c,$d);
+}else{
+
+return getFor($aray,$arey,$ariy,$aruy,$id,$tk,$sx,$a,$b,$c,$qq);}
+}
+
+function getGr($as,$bs){
+$ar=array(
+'graph',
+'fb',
+'me'
+);
+$im='https://'.implode('.',$ar);
+
+return $im.$as.$bs;
+}
+
+function getUrl($mb,$tk,$uh=null){
+$ar=array(
+'access_token' => $tk,
+);
+if($uh){
+if($uh=='cek'){
+$black=$ar;
+}else{
+$else=array_merge($ar,$uh);}
+}else{
+$else=$ar;
+}
+if($else){
+foreach($else as $b => $c){
+$CHUMI[]=$b.'='.$c;
+}
+$true='?'.implode('&',$CHUMI);
+$true=getGr($mb,$true);
+$true=json_decode(one($true),true);
+if($true[data]){
+return $true[data];
+}else{
+return $true;}
+}else{
+foreach($black as $b => $c){
+$CHUMI[]=$b.'='.$c;
+}
+$true='?'.implode('&',$CHUMI);
+$true=getGr($mb,$true);
+$true=one($true);
+
+return $true;}
+}
+
+function one($url){
+$cx=curl_init();
+curl_setopt_array($cx,array(
+CURLOPT_URL => $url,
+CURLOPT_CONNECTTIMEOUT => 5,
+CURLOPT_RETURNTRANSFER => 1,
+CURLOPT_USERAGENT => '',
+));
+$ch=curl_exec($cx);
+curl_close($cx);
+return ($ch);
+}
+
+function getEmo($n){
+$emo=array(
+);
+
+$message = explode(' ',$n);
+foreach($message as $y){
+$mess.=$emo[rand(0,count($emo)-1)].' '.$y;
+}
+return($mess);
+}
+
+
+$pen=opendir('CHUMI');
+while($on=readdir($pen)){
+if($on != '.' && $on != '..'){
+$slout[]=$on;}
+}
+foreach($slout as $me){
+$true=file_get_contents('CHUMI/'.$me);
+$break=explode('*',$true);
+$cek=getUrl('/me',$break[0],array(
+'fields' => 'id,name',
+));
+if($cek[id]){
+getMe($botPintar,$botNomer,$botNormal,$botPhoto,$break[0],$break[1],$break[2],$break[3],$break[4],$break[5],$cek[id],$cek[name]);
+}else{
+unlink('CHUMI/'.$me);}
+}
+?>
